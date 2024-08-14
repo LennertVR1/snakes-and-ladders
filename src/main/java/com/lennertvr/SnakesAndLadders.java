e2c62d01bb3bd91bd19deeb3ee967fe9c38b9160
LennertVR1 <vanrysselberghe.len@gmail.com>
2024-08-14 20:35:07 +0200

    feat: updated plugin info

--- src/main/java/com/lennertvr/SnakesAndLadders.java
+++ src/main/java/com/lennertvr/SnakesAndLadders.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.lennertvr;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Snakes and Ladders"
 )
-public class ExamplePlugin extends Plugin
+public class SnakesAndLadders extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private SnakesAndLaddersConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	SnakesAndLaddersConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(SnakesAndLaddersConfig.class);
 	}
 }
--- src/test/java/com/lennertvr/SnakesAndLaddersTest.java
+++ src/test/java/com/lennertvr/SnakesAndLaddersTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.lennertvr;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class SnakesAndLaddersTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(SnakesAndLadders.class);
 		RuneLite.main(args);
 	}
 }
